04ac2aeb794cc2d3081857b509568b7f01bada8f
layanakhras <101355989+layanakhras@users.noreply.github.com>
2025-05-28 11:46:30 -0400

    Update proj2.cpp

--- proj2.cpp
+++ proj2.cpp
@@ -1,10 +1,7 @@
 /*****************************************
 ** File: proj2.cpp
-** Project: CMSC 202 Project 2, Spring 2021
+** Project: No Man's Alchemy
 ** Author: Layan Akhras
-** Date: 3/11/21
-** Section: 50/51
-** E-mail: lakhras1@umbc.edu
 **
 ** This file contains the main driver program for
 ** project 2. It runs the entire game in the main
